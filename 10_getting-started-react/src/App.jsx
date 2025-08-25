c6701c0a1b0f1fcf324b0a91220df8e2e709daca
rolandomajait1-rgb <rolandomajait1@gmail.com>
2025-08-26 01:13:48 +0800

    “Activity 10: Add React About Me project”

---
+++ 10_getting-started-react/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="container">
+      <h1>About Me</h1>
+      <ul>
+        <li><strong>Name:</strong> Rolando M Majait</li>
+        <li><strong>Course/Year:</strong> BS Information System 3rd Year</li>
+      </ul>
+
+      <h2>Fun Fact</h2>
+      <p>I can dance without music but feel the beats.</p>
+
+      <h2>Why I Want to Learn Git & GitHub</h2>
+      <p>
+        I want to learn Git and GitHub because they are helpful when it comes to managing and tracking code, and it is required for software developers, web developers, and professionals in the IT industry. They also make it easier to work and collaborate with other people on the same project. With GitHub, I can store my code online, share it, and even work on projects with others around the world. Learning these tools will help me become a more effective and organized developer.
+      </p>
+    </div>
+  )
+}
+
+export default App
